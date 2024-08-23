77c90f5f3e5e479e569e1046e9db9b8aee9f07b5
leyton.graphicart <leyton_espitia@hotmail.com>
2024-08-23 21:33:07 +0000

    parametrización del nombre del archivo y ruta

--- exercises/6-multiplication-app/src/app.ts
+++ exercises/6-multiplication-app/src/app.ts
@@ -7,7 +7,7 @@ import { ServerApp } from "./presentation/server-app";
 })();
 
 async function main() {
-    const { b: base, l: limit, s: showTable } = args;
-    ServerApp.run({ base, limit, showTable });
+    const { b: base, l: limit, s: showTable, n: fileName, d: fileDestination } = args;
+    ServerApp.run({ base, limit, showTable, fileName, fileDestination });
 }
 
